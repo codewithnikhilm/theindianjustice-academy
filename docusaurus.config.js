d1c0197a2be0f7ea012b013c13fa11e5575499ca
Nikhil M <47181632+codewithnikhilm@users.noreply.github.com>
2026-08-11 23:27:08 +0530

    added analyticals

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,6 +57,10 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-1X7VN4C0RB',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
